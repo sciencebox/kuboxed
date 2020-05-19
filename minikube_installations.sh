a14b4b0a01003c298074b9e256d5bf1078685ecf
ashwath7500 <39669214+ashwath7500@users.noreply.github.com>
2020-05-19 17:40:04 +0530

    Update minikube_installations.sh

--- minikube_installations.sh
+++ minikube_installations.sh
@@ -2,4 +2,4 @@
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64   && chmod +x minikube
 sudo mkdir -p /usr/local/bin/
 sudo install minikube /usr/local/bin/
-sudo minikube start
+sudo minikube start --driver=none
