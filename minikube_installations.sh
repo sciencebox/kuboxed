8c19b2128dc01b7c3a5ec754124fc5c23a88c216
ashwath7500 <39669214+ashwath7500@users.noreply.github.com>
2020-05-19 17:35:15 +0530

    minikube installation

---
+++ minikube_installations.sh
@@ -0,0 +1,5 @@
+#install minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64   && chmod +x minikube
+sudo mkdir -p /usr/local/bin/
+sudo install minikube /usr/local/bin/
+sudo minikube start
