a45a130d71c36a1710ae23bf8112214c821e141f
ashwath7500 <39669214+ashwath7500@users.noreply.github.com>
2020-05-20 18:42:31 +0530

    Delete minikube_installations.sh

--- minikube_installations.sh
+++
@@ -1,5 +0,0 @@
-#install minikube
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64   && chmod +x minikube
-sudo mkdir -p /usr/local/bin/
-sudo install minikube /usr/local/bin/
-sudo minikube start --driver=none
